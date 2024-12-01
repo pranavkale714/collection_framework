dde267aceeb88c2a7675a787e8f72446ff5c8973
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-01 20:43:12 +0530

    Add files via upload

---
+++ What_is_DS_1st/src/What_is_DS/What_is_DS.java
@@ -0,0 +1,36 @@
+package What_is_DS;
+
+public class What_is_DS {
+
+	public static void main(String[] args) {
+		System.out.println("Data Structure-Way to store Data in efficient manner for retrival and access.");
+		
+		System.out.println("DS and Algorithm-When we talk time and space complexity.");
+		
+		System.out.println("Types of DS-");
+		System.out.println(" 1.Primitive-");
+		System.out.println("\t a.Boolean");
+		System.out.println("\t b.Char");
+		System.out.println("\t c.Int");
+		System.out.println("\t d.Float ");
+		System.out.println("\t e.Double");
+		System.out.println("\t f.Short");
+		System.out.println("\t g.long");
+		System.out.println("\t h.byte");
+		System.out.println(" 2.Non Primitive-");
+		System.out.println("    i.Linear DS-");
+		System.out.println("\t a.String");
+		System.out.println("\t b.Arrays");
+		System.out.println("\t c.List,Set,Queue");
+		System.out.println("\t d.Hash set,Linked hash set etc");
+		System.out.println("    ii.Non Linear DS-");
+		System.out.println("\t a.Graph");
+		System.out.println("\t b.Trees");
+		
+		
+		
+		
+
+	}
+
+}
