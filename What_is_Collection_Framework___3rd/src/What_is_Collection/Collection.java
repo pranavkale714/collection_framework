78133f6bdc035137f6d5db939afe5c26f7d9b268
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-03 22:42:55 +0530

    Add files via upload

---
+++ What_is_Collection_Framework___3rd/src/What_is_Collection/Collection.java
@@ -0,0 +1,31 @@
+package What_is_Collection;
+
+public class Collection {
+
+	public static void main(String[] args) {
+		
+	    System.out.println("*Collection(interface)");
+	    
+        System.out.println("1.List(Interface)");
+        System.out.println("a.Array List(Class)");
+        System.out.println("b.Link List(Class)");
+        System.out.println("c.vector List(Interface)");
+        System.out.println("c.1.Stack");
+        
+
+        System.out.println("2.Set(Interface)");
+        System.out.println("a.Hash Set");
+         System.out.println("a.1.Linked hash set");
+        System.out.println("b.Sorted Set");
+         System.out.println("b.1.Navigable set");
+          System.out.println("b.2.tree set");
+          
+          
+        System.out.println("3.Queue List(Class)");
+        System.out.println("a.Priority Queue(class)");
+        System.out.println("b.Dqueue");
+         System.out.println("b.1.Array Queue(Class)");
+
+	}
+
+}
