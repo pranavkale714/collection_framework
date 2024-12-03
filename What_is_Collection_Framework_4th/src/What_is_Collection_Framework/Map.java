32e268b553d041fa440a21b443368b1df5cb0c89
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-04 00:14:51 +0530

    Add files via upload

---
+++ What_is_Collection_Framework_4th/src/What_is_Collection_Framework/Map.java
@@ -0,0 +1,19 @@
+package What_is_Collection_Framework;
+
+public class Map {
+
+	public static void main(String[] args) {
+	System.out.println("*Map");
+		    System.out.println("1.Hash Map");
+		         System.out.println("a.Linked Hash map"); 
+		    System.out.println("2.Identity hash map");
+		    System.out.println("3.weak hash map");
+		    System.out.println("4.Sorted Map");
+		         System.out.println("a.Navigable map"); 
+		             System.out.println("1.a.Tree map"); 
+		    System.out.println("5.Hash Table");
+		               System.out.println("a.Property");
+		    System.out.println("6.Dictionary");
+	}
+
+}
