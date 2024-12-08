20da3ddcff132703373900b9e83163cd1d5842d6
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-08 23:35:45 +0530

    Add files via upload

---
+++ List_And_Set_Difference_8th/src/List_and_Set/Difference.java
@@ -0,0 +1,22 @@
+package List_and_Set;
+
+public class Difference {
+
+	public static void main(String[] args) {
+	     System.out.println("1.It is index based DS");
+	     System.out.println("1.It is not,it store data according to hash code.");
+	        
+	     System.out.println("2.List can store duplicate element");
+	     System.out.println("2.Set does not allow to store duplicate element");
+	        
+	     System.out.println("3.List can store any no.of NULL value");
+	     System.out.println("3.Set can store only one null value");
+	     
+	     System.out.println("4.List Follows insertion order");
+	     System.out.println("4.Set Does not follow insertion order");
+	     
+	     System.out.println("5.We can iterate the list element by 1.list iterartor and 2.iterator");
+	     System.out.println("5.we can iterate the set element by 1.iterator");
+	}
+
+}
---
+++ List_And_Set_Difference_8th/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module List_And_Set_Difference_8th {
+}
