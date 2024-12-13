46d77441b8a8a619021ee58d33cd34c02fc3e655
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-13 23:52:56 +0530

    Add files via upload

---
+++ Array_and_ArrayList_12th/src/Array_and_ArrayList/difference.java
@@ -0,0 +1,52 @@
+package Array_and_ArrayList;
+
+public class difference {
+
+	public static void main(String[] args) {
+		System.out.println("1. Definition:");
+		System.out.println("Array: A straightforward data structure with a continuous memory location, an array stores its contents with the same name but distinct index numbers for each element of the array it contains. It is imperative that all of the data stored in an array be of the same type. After an array has been declared, its size cannot be changed.");
+		System.out.println("ArrayList: The Java collection framework contains a data structure known as an ArrayList, which is dynamic in nature. Additionally, it has components that are of the same type. In this case, it is not necessary for us to specify the length of the list.");
+
+		System.out.println("2. Static/Dynamic:");
+		System.out.println("Array: Arrays are static.");
+		System.out.println("ArrayList: ArrayList is dynamic.");
+
+		System.out.println("3. Resizable:");
+		System.out.println("Array: Fixed Length.");
+		System.out.println("ArrayList: Can be Resizable.");
+
+		System.out.println("4. Initialization:");
+		System.out.println("Array: When performing initialization for an array, it is required to specify the size of the array.");
+		System.out.println("ArrayList: It is not necessary to mention the size of an ArrayList.");
+
+		System.out.println("5. Performance:");
+		System.out.println("Array: Arrays are faster.");
+		System.out.println("ArrayList: ArrayList is slower.");
+
+		System.out.println("6. Generic Type:");
+		System.out.println("Array: An array can store primitive data as well as objects, but it cannot store generics.");
+		System.out.println("ArrayList: ArrayList is able to store generics as well as objects, but it cannot store data of primitive types.");
+
+		System.out.println("7. Iteration:");
+		System.out.println("Array: Only loops are permitted in this area.");
+		System.out.println("ArrayList: It is acceptable to use loops and iterators.");
+
+		System.out.println("8. Type Safety:");
+		System.out.println("Array: It is not type-safe.");
+		System.out.println("ArrayList: It is type-safe.");
+
+		System.out.println("9. Length:");
+		System.out.println("Array: Makes use of the length object.");
+		System.out.println("ArrayList: Make use of size() function.");
+
+		System.out.println("10. Adding Elements:");
+		System.out.println("Array: The additions are done with the assignment operator.");
+		System.out.println("ArrayList: ArrayList uses add() method for performing additions.");
+
+		System.out.println("11. Single/Multi-Dimensional:");
+		System.out.println("Array: Single and multiple dimensions are also a possibility.");
+		System.out.println("ArrayList: You are only permitted to use single dimension.");
+
+	}
+
+}
---
+++ Array_and_ArrayList_12th/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Array_and_ArrayList_12th {
+}
