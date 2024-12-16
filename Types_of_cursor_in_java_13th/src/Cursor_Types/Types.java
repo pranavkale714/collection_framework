6ac34a7b6c0ce21e8d98905cf72561962d9eba0f
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-12-16 09:37:11 +0530

    Add files via upload

---
+++ Types_of_cursor_in_java_13th/src/Cursor_Types/Types.java
@@ -0,0 +1,13 @@
+package Cursor_Types;
+
+public class Types {
+
+	public static void main(String[] args) {
+		System.out.println("Types_of_cursor");
+		System.out.println("1.Iterator");
+		System.out.println("2.ListIterator");
+		System.out.println("3.Enumeration");
+
+	}
+
+}
---
+++ Types_of_cursor_in_java_13th/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Types_of_cursor_in_java_13th {
+}
